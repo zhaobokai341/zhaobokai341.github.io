8ff6be477c0ca9981997d3fc1ac7572621c91ff9
赵博凯 <13935190240@163.com>
2025-06-22 12:17:50 +0800

    Update css_select.js

--- js/css_select.js
+++ js/css_select.js
@@ -1,29 +1,15 @@
 function changeTheme(theme) {
-    // 通过id选择器找到主题样式表
-    var link = document.getElementById('theme-style');
-    if (link) {
-        link.href = theme;
-    } else {
-        console.error('主题样式表元素未找到');
-    }
+    var link = document.querySelector('link[rel="stylesheet"]');
+    link.href = theme;
 }
 
-// 检测当前页面是否在子目录中
-function getThemePath() {
-    // 检查当前URL是否包含子目录
-    const isInSubdirectory = window.location.pathname.split('/').length > 2;
-    // 如果在子目录中，返回相对路径前缀
-    return isInSubdirectory ? '../style/' : './style/';
-}
-
-let themePath = getThemePath();
 let html = `<div id="theme-menu">
     <label for="theme">选择主题：</label>
     <select id="theme" onchange="changeTheme(this.value)">
-        <option value="${themePath}rainbow.css">彩虹色</option>
-        <option value="${themePath}white.css">亮色</option>
-        <option value="${themePath}dark.css">暗色</option>
-        <option value="${themePath}zhaobokai.css">赵博凯</option>
+        <option value="/style/rainbow.css">彩虹色</option>
+        <option value="/style/white.css">亮色</option>
+        <option value="/style/dark.css">暗色</option>
+        <option value="/style/zhaobokai.css">赵博凯</option>
     </select>
 </div>`
 
